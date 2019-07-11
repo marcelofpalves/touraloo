278885fdc8dd19f5bf2cb89f4bdafdd0b9dc8fba
Marcelo Alves <alves@libra.co.jp>
2019-07-11 14:42:25 +0900

    Heroku.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("hello world"));
+app.get("/", (req, res) => res.send("hello world!"));
 
 app.listen(3001);
